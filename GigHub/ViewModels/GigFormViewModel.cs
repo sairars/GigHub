f83aa24a853f5f305f49dfee259efd5c434d4791
Saira Rashid <saira.rashid1216@gmail.com>
2023-02-08 14:03:25 -0500

    Implement server-side and basic client-side validation

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -7,23 +7,27 @@ namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required] 
         public string Venue { get; set; }
 
+        [Required] 
+        [FutureDate]
         public string Date { get; set; }
-        
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
 
-        [Display(Name = "Genre")]
+        [Display(Name = "Genre")] 
         public byte GenreId { get; set; }
-        
+
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime
+        public DateTime GetDateTime()
         {
-            get
-            {
-                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
-        } 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
+
+
