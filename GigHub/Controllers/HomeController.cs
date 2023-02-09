27219b692372e7b506a5cbd70f49ac8d8ca51e96
Saira Rashid <saira.rashid1216@gmail.com>
2023-02-09 12:57:48 -0500

    Add Name field to Artist

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,16 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
