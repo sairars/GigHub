96d0e23ecae24220bca122cfa4267499b575abb5
Saira Rashid <saira.rashid1216@gmail.com>
2023-02-10 17:34:24 -0500

    Feature: Build functionality to add gig to calendar

---
+++ GigHub/Controllers/api/AttendancesController.cs
@@ -0,0 +1,53 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public IEnumerable<Attendance> GetAttendance()
+        {
+
+
+            var attendances = _context.Attendances.ToList();
+            return attendances;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var attendeeId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == attendeeId &&
+                                              a.GigId == attendanceDto.GigId))
+                return BadRequest("The attendance already exists");
+
+            var attendance = new Attendance
+            {
+                GigId = attendanceDto.GigId,
+                AttendeeId = attendeeId
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- GigHub/Global.asax.cs
+++ GigHub/Global.asax.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace GigHub
 {
@@ -12,8 +10,10 @@ namespace GigHub
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
