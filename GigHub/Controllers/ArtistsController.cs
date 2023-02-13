cb56590497a0657d757fcb4aa7a1796353732f2c
Saira Rashid <saira.rashid1216@gmail.com>
2023-02-13 13:02:06 -0500

    Feature: implement view artistd im following page

---
+++ GigHub/Controllers/ArtistsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using GigHub.Models;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class ArtistsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtistsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Artists
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var artists = _context.Followings
+                                                    .Where(f => f.FollowerId == userId)
+                                                    .Select(f => f.Artist)
+                                                    .ToList();
+            return View(artists);
+        }
+    }
+}
